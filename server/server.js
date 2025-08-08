be7f19a4840ff44028e2cd202d135a94033005c5
SabbirMeazee <mdsabbirmeazee69@gmail.com>
2025-08-08 15:11:16 +0600

    server added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
+
+
 const app = express();
 const port = 3000;
 
@@ -18,5 +22,6 @@ app.use(clerkMiddleware())
 app.get('/', (req, res) => res.send('Server is Live!'))
 
 // API Routes
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
